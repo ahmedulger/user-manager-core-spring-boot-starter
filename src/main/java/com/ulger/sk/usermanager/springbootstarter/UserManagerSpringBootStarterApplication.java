8c2ad49cbd0a68114148b9bb5a872d904d7f556b
kafeinaulger <ahmet.ulger@vodafone.com>
2020-11-10 21:24:55 +0300

    Initial

---
+++ src/main/java/com/ulger/sk/usermanager/springbootstarter/UserManagerSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.ulger.sk.usermanager.springbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagerSpringBootStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagerSpringBootStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ulger/sk/usermanager/springbootstarter/UserManagerSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ulger.sk.usermanager.springbootstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagerSpringBootStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
